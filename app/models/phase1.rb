ed46b86e3d582e7da2ca324480e5c77b92367da6
jonathanmpope <jonathanmpope@gmail.com>
2022-07-13 20:41:01 -0600

    test:phase one validation tests

--- app/models/phase1.rb
+++ app/models/phase1.rb
@@ -1,3 +1,12 @@
 class Phase1 < ApplicationRecord
     belongs_to :person
+    validates_presence_of :protein_grams
+    validates_presence_of :protein_cals
+    validates_presence_of :protein_percent
+    validates_presence_of :carb_grams
+    validates_presence_of :carb_cals
+    validates_presence_of :carb_percent
+    validates_presence_of :fat_grams
+    validates_presence_of :fat_cals
+    validates_presence_of :fat_percent
 end 
--- spec/models/phase1_spec.rb
+++ spec/models/phase1_spec.rb
@@ -1,12 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe Person, type: :model do 
+RSpec.describe Phase1, type: :model do 
     describe 'relationships' do
         it { should belong_to :person }
     end 
 
     describe 'validations' do 
-        it { should validate_presence_of :name }
+        it { should validate_presence_of :protein_grams }
+        it { should validate_presence_of :protein_cals }
+        it { should validate_presence_of :protein_percent }
+        it { should validate_presence_of :carb_grams }
+        it { should validate_presence_of :carb_cals }
+        it { should validate_presence_of :carb_percent }
+        it { should validate_presence_of :fat_grams }
+        it { should validate_presence_of :fat_cals }
+        it { should validate_presence_of :fat_percent }
     end 
 
     describe 'instance methods' do 
