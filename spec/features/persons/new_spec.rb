35560c808e0d50cc0510df9a814cec0559d0cc3f
jonathanmpope <jonathanmpope@gmail.com>
2022-07-19 13:50:43 -0600

    feat: add lean mass calc with test for person

--- spec/features/persons/new_spec.rb
+++ spec/features/persons/new_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'the person new page' do
         expect(page).to have_content("BTE Nutrition Calculator")
     end  
 
-    it 'should have an empty page with lots of forms' do
+    it 'should have forms you can fill out' do
         visit '/'
 
         fill_in("Name", with:"Testy")
@@ -22,5 +22,21 @@ RSpec.describe 'the person new page' do
 
         expect(page).to have_content("Testy")
     end 
+
+    it 'should not allow you to submit a form with an empty area' do
+        visit '/'
+
+        fill_in("Name", with:"Testy")
+        fill_in("Email", with:"test@test.com")
+        fill_in("Weight", with:180)
+        choose(id="sex_male")
+        choose(id="bodycomp_10")
+        choose(id="activity_level_moderate")
+        choose(id="training_load_9-12")
+        choose(id="fc_pref_fat")
+        click_button("Submit")
+
+        expect(current_path).to eq('/')
+    end
 
 end 
