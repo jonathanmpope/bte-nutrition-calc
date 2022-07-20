679c588fd996e5289bb9e4ada38298eb6f20d478
jonathanmpope <jonathanmpope@gmail.com>
2022-07-19 20:04:43 -0600

    feat: updated styling with additional macro tables

--- app/controllers/person_phases_controller.rb
+++ app/controllers/person_phases_controller.rb
@@ -1,8 +1,10 @@
 class PersonPhasesController < ApplicationController 
     
     def create
-        @person = Person.find(params[:person_id])
-        @person.calculations
+        @person1 = Person.find(params[:person_id])
+        @person1.calculations
+        @phase1 = @person1.phases[0]
+        # Phase.where("person_id = ?", @person1.id)
     end 
 
 end 
--- app/models/phase.rb
+++ app/models/phase.rb
@@ -10,4 +10,28 @@ class Phase < ApplicationRecord
     validates_presence_of :fat_cals
     validates_presence_of :fat_percent
     validates_presence_of :name
+
+    def macros_per_meal_3
+        three_meals = {}
+        three_meals[:protein_grams] = (protein_grams / 3.0)
+        three_meals[:carb_grams] = (carb_grams / 3.0)
+        three_meals[:fat_grams] = (fat_grams / 3.0)
+        three_meals
+    end
+    
+    def macros_per_meal_4
+        four_meals = {}
+        four_meals[:protein_grams] = (protein_grams / 4.0)
+        four_meals[:carb_grams] = (carb_grams / 4.0)
+        four_meals[:fat_grams] = (fat_grams / 4.0)
+        four_meals
+    end
+
+   def macros_per_meal_5
+        five_meals = {}
+        five_meals[:protein_grams] = (protein_grams / 5.0)
+        five_meals[:carb_grams] = (carb_grams / 5.0)
+        five_meals[:fat_grams] = (fat_grams / 5.0)
+        five_meals
+    end
 end 
