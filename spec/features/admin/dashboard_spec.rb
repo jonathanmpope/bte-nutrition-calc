abc6150185038d668ceabb0339e63d99bb43d970
jonathanmpope <jonathanmpope@gmail.com>
2022-09-07 15:38:23 -0600

    feat: add pending admins and users links to admin dashboard with test

--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -2,20 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Admin Dashboard page" do
   it "allows you to view content if you're an approved admin" do
-    visit '/admins/login'
-
     admin = Admin.create!(username:"funbucket13", email: 'test@test.com', password: "test", status: 1)
     
-    username = "funbucket13"
-    password = "test"
-    email = 'test@test.com'
+    allow_any_instance_of(ApplicationController).to receive(:current_admin).and_return(admin)
 
-    fill_in :username, with: username
-    fill_in :password, with: password
+    visit '/admins/dashboard'
 
-    click_on "Login"
-
-    expect(page).to have_content("Welcome, #{username}!")
+    expect(page).to have_content("Welcome, funbucket13!")
   end
 
   it "will tell you that you need to log in" do
@@ -24,6 +17,19 @@ RSpec.describe "Admin Dashboard page" do
     visit '/admins/dashboard'
 
     expect(page).to have_content("Sorry, you must logged in.")
+    expect(current_path).to eq("/admins/login")
+  end
+
+  it "has links to logout and to view user data and admins pending approval" do
+    admin = Admin.create!(username:"funbucket13", email: 'test@test.com', password: "test", status: 1)
+    
+    allow_any_instance_of(ApplicationController).to receive(:current_admin).and_return(admin)
+
+    visit '/admins/dashboard'
+
+    expect(page).to have_content("Logout")
+    expect(page).to have_content("User Data")
+    expect(page).to have_content("Pending Admins")
   end
 
   xit "has a list of admins pending approval" do
