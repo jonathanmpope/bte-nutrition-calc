8fa839a5eb5852bc79a03b52b4a63d2bd09c8561
jonathanmpope <jonathanmpope@gmail.com>
2022-11-07 09:55:38 -0700

    refactor mailchimp facade for put request

--- app/facades/mailchimp_facade.rb
+++ app/facades/mailchimp_facade.rb
@@ -1,52 +1,25 @@
 class MailchimpFacade
 
     def self.add_subscriber(user)
-        mailchimp = MailchimpService.create_update_user(user)
-        binding.pry 
-        # list_id = ENV["mailchimp_list_id"]
+        user_details = {
+        email_address: user.email,
+        status: "subscribed",
+        merge_fields: {
+            FNAME: user.name,
+            LNAME: "Test",
+        },
+        };
 
-        # subscribing_user = {
-        #     first_name: "Apple",
-        #     last_name: "Sauce",
-        #     email_address: "applesauce@example.com"
-        # }
-        # binding.pry 
-        # response = mailchimp.lists.add_list_member list_id, {
-        #     email_address: subscribing_user[:email_address],
-        #     status: "subscribed",
-        #     merge_fields: {
-        #         FNAME: subscribing_user[:first_name],
-        #         LNAME: subscribing_user[:last_name]
-        #         },
-        #     }
-        # binding.pry 
+        conn = Faraday.new(
+            url: "https://#{ENV['mailchimp_server']}.api.mailchimp.com/3.0/lists/#{ENV['mailchimp_list_id']}/members/#{user.email}",
+            headers: {'Content-Type' => 'application/json', 'Authorization': "Bearer #{ENV['mailchimp_api_key']}"}
+        )
+        
+        response = conn.put() do |req|
+            req.body = user_details.to_json
+        end
 
-        # begin
-        #     mailchimp = MailchimpMarketing::Client.new
-        #     mailchimp.set_config({
-        #         :api_key => ENV["mailchimp_api_key"],
-        #         :server => ENV["mailchimp_server"]
-        #     })
-        #     response =
-        #         mailchimp.lists.set_list_member(
-        #         ENV["mailchimp_list_id"],
-        #         'subscriber_hash',
-        #         {   
-        #             'email_address' => user.email,
-        #             'status_if_new' => 'subscribed'
-        #         },
-        #         'merge_fields': 
-        #         {
-        #             'FNAME' => user.name,
-        #             'LNAME' => "Pope"
-        #         }
-        #         )
-        #     p response
-        #     binding.pry 
-        # rescue MailchimpMarketing::ApiError => e
-        #     puts "Error: #{e}"
-        # end
-
+        response = JSON.parse(response.body)
     end 
 
 end 
